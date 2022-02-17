6026a529230c6984792e8908ade858557b3aa8b2
Debs SSimão <54463178+deborasimao@users.noreply.github.com>
2022-02-17 01:18:45 -0300

    Add files via upload

---
+++ 22_par_impar.py
@@ -0,0 +1,6 @@
+valor = int(raw_input('Informe um numero: '))
+
+if (valor % 2 == 0):
+    print 'Valor eh par'
+else:
+    print 'Valor eh impar'
---
+++ 23_inteiro_decimal.py
@@ -0,0 +1,6 @@
+valor = float(raw_input('Informe um numero: '))
+
+if (valor == int(valor)):
+    print 'Valor eh inteiro'
+else:
+    print 'Valor eh decimal'
